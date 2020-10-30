f10a3fc8db1e111b769dd9352e0178ef027c599e
DanilShishkin <71436651+DanilShishkin@users.noreply.github.com>
2020-10-30 20:22:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include iostream
+#include bag.h
+#include string
+
+using namespace std;
+int main(){ 
+    double m = supermaxdouble(1.2, 3.14);
+    Bagint,10 small_bag;
+    Bagstring bag_of_words;
+    Bag double_bag;
+    cout  double_bag[3]  endl;
+    small_bag[5] = 12;
+    bag_of_words[1] = 123;
+    cout  small_bag[5]     bag_of_words[1]  endl;
+    Bagchar,15 mystring;
+    Bagchar,5 mystring2;
+    mystring[11] = 'e';
+    mystring2[11] = 'e';
+    BagNoDefault,2 ndb;
+    NoDefault d;
+    NoDefault dq[12];
+    cout  mystring[9]  endl;
+    cout  GetdoubleSize  endl;
+    cout  GetintSize  endl;
+    return 0;
+}
