47b92c04b3d12587badac47020c3acd804978805
DanilShishkin <dashishkin_1@miem.hse.ru>
2020-11-03 11:10:21 +0300

    Оно не работает, потому что компилятор неявно приводит мой класс к типу вектор.

--- Prac/03.11.20/main.cpp
+++ Prac/03.11.20/main.cpp
@@ -1,28 +1,83 @@
 #include <iostream>
+#include <vector>
 #include <type_traits>
 
+void empty(int top){
+    if (top == -1){
+        throw "Stack is empty";
+    }
+}
+
 template <typename T>
-class Stack{
-public:
-    Stack(){
-        max_size = 10;
-        stack[max_size - 1];
-    }
-    Stack(int max_size_){
-        max_size = max_size_;
-    }
+void arithmetic(){
+    if (not std::is_arithmetic_v<T>){
+        throw "Typename should be arithmetic type";
+}
 
-    template<typename T>
-    push(T){
-
-    }
+template <typename T>
+class Stack
+{
 private:
-    int max_size = 0;
     int top = -1;
-    T stack;
+    int max_size;
+    std::vector<T> stack;
+public:
+    Stack(int max_size){
+        try{
+            std::vector<T> stack(max_size);
+            arithmetic<T>(T tmp);
+        }
+        catch (const char* exception) {
+            std::cout << exception;
+        }
+    }
+    void push(T);
+    T pop();
+
+    void overflow(){
+        if (stack.top == max_size){
+            throw "Stack is overflowed";
+        }
+    }
 };
 
-int main(){
-    Stack<int> stack(10);
+
+template <typename T>
+void Stack<T>::push(const T value)
+{
+    try {
+        stack.overflow();
+        stack.push_back(value);
+        top++;
+    }
+    catch (const char* exception) {
+        std::cout << exception;
+    }
+}
+
+template <typename T>
+T Stack<T>::pop()
+{
+    try{
+        empty(stack.pop);
+        T ret = stack[top--];
+        stack.pop_back();
+        return ret;
+    }
+    catch (const char* exception) {
+        std::cout << exception;
+    }
+}
+
+
+int main()
+{
+    Stack<int> s(5);
+    s.push(5);
+    std::cout << s.pop() << std::endl;
+    s.pop();
+    s.push(3);
+    std::cout << s.pop() << std::endl;
+
     return 0;
 }
